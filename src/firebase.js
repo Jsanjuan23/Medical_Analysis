0c8f238b3d157dc8409672ba6d0be86f4bf83616
El coco <jesussanjuan23@gmail.com>
2022-10-29 00:08:37 -0500

    Software 80%

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCELaEe6U5zQCb41R-1rL1xO8wQuSdlTB4",
+    authDomain: "analisis-a29ee.firebaseapp.com",
+    projectId: "analisis-a29ee",
+    storageBucket: "analisis-a29ee.appspot.com",
+    messagingSenderId: "665397395756",
+    appId: "1:665397395756:web:eb0587510b16430326ac89"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export{firebase}
+
+  
